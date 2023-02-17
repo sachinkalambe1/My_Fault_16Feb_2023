299691b800c82c057e5dd8c0e21c09dcb89a5e39
Sachin kalambe <kalambesachin2000@gmail.com>
2023-02-17 10:46:16 +0530

    perimeter and area of rectangle

---
+++ Primeter_andArea_Rectangle.java
@@ -0,0 +1,12 @@
+class Primeter_andArea_Rectangle{
+public static void main(String[]args){
+
+int height=7;
+int width=5;
+double area=height*width;
+double perimetre=2*(height+width);
+System.out.println(area);
+System.out.println(perimetre);
+
+}
+}
