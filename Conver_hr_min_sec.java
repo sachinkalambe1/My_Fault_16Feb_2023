96d300f15693fb02c1b8de3bc0199533c029f36b
Sachin kalambe <kalambesachin2000@gmail.com>
2023-02-17 17:23:05 +0530

    Conver-hr_min_sec

---
+++ Conver_hr_min_sec.java
@@ -0,0 +1,20 @@
+// Write java programme to convert a given integer in sec into hour min and sec.
+
+import java.util.*;
+class Conver_hr_min_sec{
+public static void main(String[]args){
+
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter integer value in sec");
+
+int sec=Sc.nextInt();
+ int year=sec/60;
+ sec=sec%60;
+ int min=sec/60;
+ sec=sec%60;
+
+ System.out.println(year);
+ System.out.println(min);
+ System.out.println(sec);
+}
+}
