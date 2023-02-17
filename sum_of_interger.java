3d03f9d5f17aa78083bb8f6dde3c679fb6514ecc
Sachin kalambe <kalambesachin2000@gmail.com>
2023-02-17 11:56:51 +0530

    sum of two integer

---
+++ sum_of_interger.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class sum_of_interger{
+public static void main(String[]args)
+{
+
+Scanner Sc=new Scanner(System.in);
+System.out.println("Enter 1st integer number");
+int num1=Sc.nextInt();
+
+System.out.println("Enter 2nd integer number ");
+int num2=Sc.nextInt();
+
+int sum=num1+num2;
+System.out.println(sum);
+
+}
+}
